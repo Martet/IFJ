0427e8742047171f7c5303b5c10d7393d3740f87
Martet <martin@zmitko.cz>
2021-12-06 01:40:27 +0100

    Expression parser reformatting and refactorization so it isn't as painfull to look at

--- src/expression.c
+++ src/expression.c
@@ -28,9 +28,7 @@ const char Precedence_table[TABLE_SIZE][TABLE_SIZE] = {
     {'>','>','<','>','<','>','>','>','<'},  // #
 };
 
- Stack s;
-
-
+Stack s;
 
 int stack_to_table(Stack *s)
 {
@@ -42,90 +40,67 @@ int stack_to_table(Stack *s)
         {
             topicek = topicek->next;
         }
-        A= topicek->type;
+        A = topicek->type;
     }
     
-    int index = -1;
     switch (A)
     {
     case I_ID: case I_NUMBER: case I_STRING: case I_INTEGER:
-        index = 4;
-        break;                
+        return 4;              
     case I_PLUS: case I_MINUS:
-        index = 0;
-        break;
+        return 0;
     case I_MULTIPLAY: case I_DIVIDE:  case I_DIVIDE_INT:
-        index = 1;
-        break;
+        return 1;
     case I_PAR_L:
-        index = 2;
-        break;
+        return 2;
     case I_PAR_R:
-        index = 3;
-        break;
+        return 3;
     case I_DOLAR:
-        index = 5;
-        break;
+        return 5;
     case I_LESS: case I_LESS_EQ: case I_GREATER: case I_GREATER_EQ: case I_EQ: case I_EQ_NIL:
-        index = 6;
-        break;
+        return 6;
     case I_HASH:
-        index = 8;
-        break;
+        return 8;
     case I_CONCAT:
-        index = 7;
-        break;
+        return 7;
     default:
-        break;
+        return -1;
     }
-    return index;
-
 }
+
 int get_index_to_table(TokenType type)
 {
-    int index = -1;
     switch (type)
     {
     case T_ID: case T_NUMBER: case T_INTEGER: case T_STRING:
-        index = 4;
-        break;
+        return 4;
     case T_ADD:  case T_SUB:
-        index = 0;
-        break;
+        return 0;
     case T_MUL:  case T_DIV:  case T_DIV_INT:
-        index = 1;
-        break;
+        return 1;
     case T_PAR_L:
-        index = 2;
-        break;
+        return 2;
     case T_PAR_R:
-        index = 3;
-        break;
+        return 3;
     case T_EOL:
-        index = 5;
-        break;
+        return 5;
     case T_LESS: case T_LESS_EQ: case T_GREATER: case T_GREATER_EQ: case T_EQ: case T_EQ_NIL:
-        index = 6;
         boolen = true;
-        break;
+        return 6;
     case T_HASH:
-        index = 8;
-        break;
+        return 8;
     case T_CONCAT:
-        index = 7;
-        break;
+        return 7;
     default:
-        break;
+        return -1;
     }
-    return index;
-
 }
+
 IdentType TokentoIden(TokenType type){
     switch (type)
     {
     case T_ID:
         return I_ID;
-        break;
     case T_NUMBER:
         return I_NUMBER;
     case T_STRING:
@@ -134,62 +109,40 @@ IdentType TokentoIden(TokenType type){
         return I_INTEGER;
     case T_ADD:
         return I_PLUS;
-        break;
     case T_SUB:
         return I_MINUS;
-        break;
     case T_MUL:
         return I_MULTIPLAY;
-        break;
     case T_DIV:
         return I_DIVIDE;
-        break;
     case T_DIV_INT:
         return I_DIVIDE_INT;
-        break;
     case T_PAR_L:
         return I_PAR_L;
-        break;
     case T_PAR_R:
         return I_PAR_R;
-        break;
     case T_EOL:
         return I_DOLAR;
-        break;
-        break;
     case T_LESS:
         return I_LESS;
-        break;
     case T_LESS_EQ:
         return I_LESS_EQ;
-        break;
     case T_GREATER:
         return I_GREATER;
-        break;  
     case T_GREATER_EQ:
         return I_GREATER_EQ;
-        break;
     case T_EQ:
         return I_EQ;
-        break;
     case T_EQ_NIL:
         return I_EQ_NIL;
-        break;
     case T_CONCAT:
         return I_CONCAT;
-        break;
     case T_HASH:
         return I_HASH;
-        break;
     default:
-        break;
+        return I_EROR;
     }
-    return I_EROR;
-
 }
-
-
-
 
 void reduce(Stack* stack,IdentType typevar)
 {
@@ -202,7 +155,7 @@ void reduce(Stack* stack,IdentType typevar)
         Stack_Pop(stack);
         Stack_Pop(stack);
         Stack_Pop(stack);
-        Stack_Push(stack,I_NON_TERM,data);
+        Stack_Push(stack, I_NON_TERM, data);
         printf("(E) --> E \n");
         return;
     }
@@ -212,7 +165,7 @@ void reduce(Stack* stack,IdentType typevar)
         topik = topik->next;
         cnt++;
     }
-    if (cnt==1 && (stack->top->type==I_ID || stack->top->type==I_NUMBER || stack->top->type==I_STRING || stack->top->type==I_INTEGER))
+    if (cnt==1 && (stack->top->type == I_ID || stack->top->type == I_NUMBER || stack->top->type == I_STRING || stack->top->type == I_INTEGER))
     {
         char* data = stack->top->data;
         Stack_Pop(stack);
@@ -221,28 +174,27 @@ void reduce(Stack* stack,IdentType typevar)
         printf("E \n");
         //generace E
     }
-    else if(cnt==2 && stack->top->type==I_NON_TERM && stack->top->next->type == I_HASH)
+    else if(cnt == 2 && stack->top->type == I_NON_TERM && stack->top->next->type == I_HASH)
     {
         if (typevar != I_STRING)
         {
             // printf("what neni stringois");
-            exit(6);
+            return 6;
         }
         
         char* data = Stack_Top_Data(stack); 
         Stack_Pop(stack);
         Stack_Pop(stack);
         Stack_Pop(stack);
-        Stack_Push(stack,I_NON_TERM,data);
+        Stack_Push(stack, I_NON_TERM, data);
         printf("#E --> E \n");
-
     }
-    else if (cnt==3 && stack->top->type==I_NON_TERM && stack->top->next->type >= I_PLUS && stack->top->next->type <= I_DIVIDE_INT && stack->top->next->next->type == I_NON_TERM)
+    else if (cnt == 3 && stack->top->type == I_NON_TERM && stack->top->next->type >= I_PLUS && stack->top->next->type <= I_DIVIDE_INT && stack->top->next->next->type == I_NON_TERM)
     {
         if ((stack->top->next->type == I_DIVIDE || stack->top->next->type == I_DIVIDE_INT) && stack->top->data[0] == '0')
         {
             // printf("pico delis nulou");
-            exit(9);
+            return 9;
         }
         char* data = NULL;
         printf("E %d E --> E \n", stack->top->next->type);
@@ -250,56 +202,52 @@ void reduce(Stack* stack,IdentType typevar)
         Stack_Pop(stack);
         Stack_Pop(stack);
         Stack_Pop(stack);
-        Stack_Push(stack,I_NON_TERM,data);
+        Stack_Push(stack, I_NON_TERM, data);
 
         //generace operace stack->top->next->data
     }
-    else if (cnt==3 && stack->top->type==I_NON_TERM && stack->top->next->type > I_DOLAR && stack->top->next->type < I_CONCAT && stack->top->next->next->type == I_NON_TERM)
+    else if (cnt == 3 && stack->top->type == I_NON_TERM && stack->top->next->type > I_DOLAR && stack->top->next->type < I_CONCAT && stack->top->next->next->type == I_NON_TERM)
     {
         char* data = NULL;
         Stack_Pop(stack);
         Stack_Pop(stack);
         Stack_Pop(stack);
         Stack_Pop(stack);
-        Stack_Push(stack,I_NON_TERM,data);
+        Stack_Push(stack, I_NON_TERM, data);
         printf("E relace.type E --> E \n");
         //generace operace stack->top->next->data
     }
-    else if (cnt==3 && stack->top->type==I_NON_TERM && stack->top->next->type == I_CONCAT && stack->top->next->next->type == I_NON_TERM)
+    else if (cnt == 3 && stack->top->type == I_NON_TERM && stack->top->next->type == I_CONCAT && stack->top->next->next->type == I_NON_TERM)
     {
         char* data = NULL;
         if (typevar != I_STRING)
         {
-            exit(6);
+            return 6;
         }
         
         Stack_Pop(stack);
         Stack_Pop(stack);
         Stack_Pop(stack);
         Stack_Pop(stack);
-        Stack_Push(stack,I_NON_TERM,data);
+        Stack_Push(stack, I_NON_TERM, data);
         printf("E teckatecka E --> E \n");
         //generace operace stack->top->next->data
     }
     else 
     {
         printf("hello chyba");
-        exit(6);
+        return 6;
     }
-    
-
-
 }
-
 
 int solvedExpression(token_t *token)
 {
     Stack_Init(&s);
-    Stack_Push(&s,I_DOLAR,NULL);
+    Stack_Push(&s, I_DOLAR, NULL);
     int typevar = 0;
     bool end = false;
     bool string = false;
-    int cnt=0;
+    int cnt = 0;
     boolen = false; //booleen
     char* Bdata;
     while (!end)
@@ -308,16 +256,14 @@ int solvedExpression(token_t *token)
         int b;
         IdentType Btype;
 
-        
         // printf("token type %d",token->type);
         if (token->type == T_STRING)
         {
             string = true;
             if (typevar == I_INTEGER || typevar == I_NUMBER)
             {
-               exit(6);
+               return 6;
             }
-            
         }
         
         if (s.top->type != I_NON_TERM)
@@ -334,19 +280,19 @@ int solvedExpression(token_t *token)
             {
                 typevar = I_STRING;
             }
-        }    
+        }
         if (token->type == T_CONCAT)
         {
             if (typevar != I_STRING)
             {
-                exit(6);
+                return 6;
             }
             
         }
         
         if (boolen == true && (token->keyword == KW_DO || token->keyword == KW_THEN))
         {
-            printf("%d ",token->keyword);
+            printf("%d ", token->keyword);
             break;
         }
         if (token->type == T_EOL || token->type == T_EOF || token->type == T_COMMA)
@@ -354,30 +300,28 @@ int solvedExpression(token_t *token)
             b = 5;
             Btype = I_DOLAR;
 
-            end= true;
+            end = true;
         }
         else
         {
-
             b = get_index_to_table(token->type);
             Btype = TokentoIden(token->type);
 
-            
             if (Btype == I_ID)
             {
                 Bdata = malloc(99);
                 if (Bdata == NULL)
                 {
-                exit(99);
+                    exit(99);
                 }
-                strcpy(Bdata,token->data);
+                strcpy(Bdata, token->data);
             }
             else if(Btype == I_INTEGER)
             {
                 Bdata = malloc(99);
                 if (Bdata == NULL)
                 {
-                exit(99);
+                    exit(99);
                 }              
                 sprintf(Bdata, "%d", token->integer);
             }
@@ -386,7 +330,7 @@ int solvedExpression(token_t *token)
                 Bdata = malloc(99);
                 if (Bdata == NULL)
                 {
-                exit(99);
+                    exit(99);
                 }            
                 strcpy(Bdata,token->data);
             }
@@ -405,57 +349,54 @@ int solvedExpression(token_t *token)
             }
             
         }
-        printf("  \n indexy  %d , %d \n",a,b);
+        //printf("  \n indexy  %d , %d \n",a,b);
         switch (Precedence_table[a][b])
         {
             case '=':
-                Stack_Push(&s,Btype,Bdata);
+                Stack_Push(&s, Btype, Bdata);
                 // printf("jsem tu = \n");
                 get_token(token);
                 break;
             case '<':
                 // printf("jsem tu <  ");
-                Stack_InsertBeforeNonTerm(&s,I_HALT,NULL); //<
-                Stack_Push(&s,Btype,Bdata);
-                Stack_Print(&s); //tisknu
+                Stack_InsertBeforeNonTerm(&s, I_HALT, NULL); //<
+                Stack_Push(&s, Btype, Bdata);
+                //Stack_print(&s); //tisknu
                 // token_print(token);
                 get_token(token);
                 break;
             case '>':
                 // printf("jsem tu >  reduction \n ");
                     // printf("typevar = %d",typevar);
-                reduce(&s,typevar);
-                Stack_Print(&s);
+                reduce(&s, typevar);
+                //Stack_print(&s);
                 break;
             case 'D':
                 // printf("redukujuu");
-                reduce(&s,typevar);
+                reduce(&s, typevar);
                 break;
             default:
                 printf("haha chyba");
-                exit(6);
+                return 6;
                 break;
         }
-
-    
     }
     // printf("konecna vystupovat \n");
-    Stack_Print(&s);
+    //Stack_print(&s);
     int a = stack_to_table(&s);
     // printf("a %d ", a);
     // printf("typevar = %d",typevar);
     while (a != 5)
     {
-        reduce(&s,typevar);
+        reduce(&s, typevar);
         a = stack_to_table(&s);
-        Stack_Print(&s);
+        //Stack_print(&s);
     }
     // printf("konecna vystupovat po while \n");
     Stack_Destroy(&s);
     // printf("konecna vystupovat po destroy \n");
     // printf("typevar %d", typevar);
-    Stack_Print(&s);
+    //Stack_print(&s);
     
     return 0;
- 
 }
